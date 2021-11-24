172473564271a0812ac4a90f7bc80ed2eda02ea3
ashaik16 <ashaik16@depaul.edu>
2021-11-24 04:27:45 -0600

    UI changes added

--- todoapp/src/User/UserBar.js
+++ todoapp/src/User/UserBar.js
@@ -1,11 +1,12 @@
 import { useContext, useState } from "react";
-import { Button, Container } from "react-bootstrap";
+import { Button, Container, Card, Row, Col, Image } from "react-bootstrap";
 import { Link, useNavigation } from "react-navi";
 import { StateContext } from "../Contexts";
 import HomePage from "../pages/HomePage";
 import Login from "./Login";
 import Logout from "./Logout";
 import Register from "./Register";
+import "../style.css";
 export default function UserBar() {
   const navigation = useNavigation();
   const { state } = useContext(StateContext);
@@ -28,24 +29,73 @@ export default function UserBar() {
     );
   } else {
     return (
-      <Container>
-        {!user.username && (
-          // <div>
-          //   <h2>
-          //     <u>Login</u>
-          //   </h2>
+      <Container
+        style={{
+          marginLeft: "11px",
+        }}
+      >
+        {/* <Card
+              className="bg-dark text-white card-width"
+              style={{ width: "75%" }}
+            >
+              <Card.Img
+                className="card-width"
+                src={process.env.PUBLIC_URL + "/MyTodoLogo.jpg"}
+                alt="Card image"
+              />
+            </Card> */}
 
-          //   <Login />
-          //   <br />
-          //   <hr />
-          //   <h1>OR</h1>
-          //   <h2>
-          //     <u>Register For New User</u>
-          //   </h2>
+        <Col
+          className="text-center"
+          style={{ paddingTop: "10px", paddingBottom: "2px" }}
+        >
+          <Card style={{ width: "60%", height: "97%" }}>
+            <Card.Header style={{ marginTop: "20px" }}>
+              {" "}
+              <h5>
+                <b>
+                  <i>Your Daily Planner</i>
+                </b>
+              </h5>
+              <Image
+                style={{
+                  width: "55%",
+                  height: "55%",
+                }}
+                src={process.env.PUBLIC_URL + "/Logo.png"}
+              />
+            </Card.Header>
+            <Card.Body>
+              <Card.Title> Already Have an Account?</Card.Title>
 
-          //   <Register />
-          // </div>
-
+              {!user.username && (
+                <div className="justify-content-end">
+                  <Button variant="link" onClick={(e) => setShowLogin(true)}>
+                    Login
+                  </Button>
+                  <Login
+                    show={showLogin}
+                    handleClose={() => setShowLogin(false)}
+                    setShowLogin={setShowLogin}
+                  />
+                </div>
+              )}
+            </Card.Body>
+            <Card.Footer className="text-muted">
+              Sign Up For New User
+              <br />
+              <Button variant="link" onClick={(e) => setShowRegister(true)}>
+                Register
+              </Button>
+              <Register
+                show={showRegister}
+                handleClose={() => setShowRegister(false)}
+                setShowRegister={setShowRegister}
+              />
+            </Card.Footer>
+          </Card>
+        </Col>
+        {/* {!user.username && (
           <div className="justify-content-end">
             <Button variant="link" onClick={(e) => setShowLogin(true)}>
               Login
@@ -64,7 +114,7 @@ export default function UserBar() {
               setShowRegister={setShowRegister}
             />
           </div>
-        )}
+        )} */}
       </Container>
     );
   }
